6acc5beae9e2286f9547b59d7c39d07f019f287c
Oliver Rossmueller <oros@apache.org>
2003-01-15 21:50:24 +0000

    reformatted + license
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/refactorings_branch@323016 13f79535-47bb-0310-9956-ffa450edef68

--- src/protocol/http/org/apache/jmeter/protocol/http/gui/HTTPArgumentsPanel.java
+++ src/protocol/http/org/apache/jmeter/protocol/http/gui/HTTPArgumentsPanel.java
@@ -1,4 +1,59 @@
+/*
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 - 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/)."
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ * "Apache JMeter" must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ * "Apache JMeter", nor may "Apache" appear in their name, without
+ * prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
 package org.apache.jmeter.protocol.http.gui;
+
 
 import java.util.Iterator;
 
@@ -8,7 +63,9 @@ import org.apache.jmeter.gui.util.PowerTableModel;
 import org.apache.jmeter.protocol.http.util.HTTPArgument;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.util.JMeterUtils;
+
 import org.apache.jorphan.collections.Data;
+
 
 /**
  * @author Administrator
@@ -16,73 +73,74 @@ import org.apache.jorphan.collections.Data;
  * To change this generated comment edit the template variable "typecomment":
  * Window>Preferences>Java>Templates.
  */
-public class HTTPArgumentsPanel extends ArgumentsPanel {
-	
-	private static final String ENCODED_VALUE = JMeterUtils.getResString("encoded_value");
-	private static final String ENCODE_OR_NOT = JMeterUtils.getResString("encode?");
-	
-	
-	protected void initializeTableModel() {
-		tableModel = new PowerTableModel(new String[]{Arguments.COLUMN_NAMES[0],Arguments.COLUMN_NAMES[1],
-				ENCODE_OR_NOT},
-				new Class[]{String.class,String.class,Boolean.class});
-	}
-	
-	public HTTPArgumentsPanel()
-	{
-		super(JMeterUtils.getResString("paramtable"));
-	}		
-	
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@return   !ToDo (Return description)
-	 ***************************************/
-	public TestElement createTestElement()
-	{
-		Data model = tableModel.getData();
-		Arguments args = new Arguments();
-		model.reset();
-		while(model.next())
-		{
-			if(((Boolean)model.getColumnValue(ENCODE_OR_NOT)).booleanValue())
-			{
-				args.addArgument(new HTTPArgument((String)model.getColumnValue(Arguments.COLUMN_NAMES[0]),
-						model.getColumnValue(Arguments.COLUMN_NAMES[1])));
-			}
-			else
-			{
-				HTTPArgument arg = new HTTPArgument();
-				arg.setAlwaysEncode(false);
-				arg.setName((String)model.getColumnValue(Arguments.COLUMN_NAMES[0]));
-				arg.setValue(model.getColumnValue(Arguments.COLUMN_NAMES[1]));
-				args.addArgument(arg);
-			}
-		}
-		this.configureTestElement(args);
-		return (TestElement)args.clone();
-	}
-	
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@param el  !ToDo (Parameter description)
-	 ***************************************/
-	public void configure(TestElement el)
-	{
-		super.configure(el);
-		if(el instanceof Arguments)
-		{
-			tableModel.clearData();
-			HTTPArgument.convertArgumentsToHTTP((Arguments)el);
-			Iterator iter = ((Arguments)el).getArguments().iterator();
-			while(iter.hasNext())
-			{
-				HTTPArgument arg = (HTTPArgument)iter.next();
-				tableModel.addRow(new Object[]{arg.getName(),arg.getValue(),
-						new Boolean(arg.getAlwaysEncode())});
-			}
-		}
-		checkDeleteStatus();
-	}
+public class HTTPArgumentsPanel extends ArgumentsPanel
+{
+
+    private static final String ENCODED_VALUE = JMeterUtils.getResString("encoded_value");
+    private static final String ENCODE_OR_NOT = JMeterUtils.getResString("encode?");
+
+
+    protected void initializeTableModel()
+    {
+        tableModel = new PowerTableModel(new String[]{Arguments.COLUMN_NAMES[0], Arguments.COLUMN_NAMES[1],
+                                                      ENCODE_OR_NOT},
+                                         new Class[]{String.class, String.class, Boolean.class});
+    }
+
+    public HTTPArgumentsPanel()
+    {
+        super(JMeterUtils.getResString("paramtable"));
+    }
+
+    /****************************************
+     * !ToDo (Method description)
+     *
+     *@return   !ToDo (Return description)
+     ***************************************/
+    public TestElement createTestElement()
+    {
+        Data model = tableModel.getData();
+        Arguments args = new Arguments();
+        model.reset();
+        while (model.next())
+        {
+            if (((Boolean)model.getColumnValue(ENCODE_OR_NOT)).booleanValue())
+            {
+                args.addArgument(new HTTPArgument((String)model.getColumnValue(Arguments.COLUMN_NAMES[0]),
+                                                  model.getColumnValue(Arguments.COLUMN_NAMES[1])));
+            } else
+            {
+                HTTPArgument arg = new HTTPArgument();
+                arg.setAlwaysEncode(false);
+                arg.setName((String)model.getColumnValue(Arguments.COLUMN_NAMES[0]));
+                arg.setValue(model.getColumnValue(Arguments.COLUMN_NAMES[1]));
+                args.addArgument(arg);
+            }
+        }
+        this.configureTestElement(args);
+        return (TestElement)args.clone();
+    }
+
+    /****************************************
+     * !ToDo (Method description)
+     *
+     *@param el  !ToDo (Parameter description)
+     ***************************************/
+    public void configure(TestElement el)
+    {
+        super.configure(el);
+        if (el instanceof Arguments)
+        {
+            tableModel.clearData();
+            HTTPArgument.convertArgumentsToHTTP((Arguments)el);
+            Iterator iter = ((Arguments)el).getArguments().iterator();
+            while (iter.hasNext())
+            {
+                HTTPArgument arg = (HTTPArgument)iter.next();
+                tableModel.addRow(new Object[]{arg.getName(), arg.getValue(),
+                                               new Boolean(arg.getAlwaysEncode())});
+            }
+        }
+        checkDeleteStatus();
+    }
 }
