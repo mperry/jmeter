ce7c83a22f7489ecda0da55d3fea115b4d5059c6
Oliver Rossmueller <oros@apache.org>
2003-01-18 14:23:15 +0000

    reformatted
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/refactorings_branch@323025 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/control/LoopController.java
+++ src/core/org/apache/jmeter/control/LoopController.java
@@ -53,10 +53,15 @@
  * <http://www.apache.org/>.
  */
 package org.apache.jmeter.control;
+
+
 import java.io.Serializable;
+
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.samplers.AbstractSampler;
+
+
 /****************************************
  * Title: JMeter Description: Copyright: Copyright (c) 2000 Company: Apache
  *
@@ -65,163 +70,162 @@ import org.apache.jmeter.samplers.AbstractSampler;
  *@version   1.0
  ***************************************/
 
-public class LoopController extends GenericController implements Serializable
-{
-	private final static String LOOPS = "LoopController.loops";
-	private final static String CONTINUE_FOREVER = "LoopController.continue_forever";
-	private int loopCount = 0;
+public class LoopController extends GenericController implements Serializable {
 
-	/****************************************
-	 * !ToDo (Constructor description)
-	 ***************************************/
-	public LoopController()
-	{
-		setContinueForever(true);
-	}
-
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@param loops  !ToDo (Parameter description)
-	 ***************************************/
-	public void setLoops(int loops)
-	{
-		setProperty(LOOPS,new Integer(loops));
-	}
-
-	/****************************************
-	 * !ToDoo (Method description)
-	 *
-	 *@return   !ToDo (Return description)
-	 ***************************************/
-	public int getLoops()
-	{
-		return getPropertyAsInt(LOOPS);
-	}
-
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@param forever  !ToDo (Parameter description)
-	 ***************************************/
-	public void setContinueForever(boolean forever)
-	{
-		setProperty(CONTINUE_FOREVER,new Boolean(forever));
-	}
+  private final static String LOOPS = "LoopController.loops";
+  private final static String CONTINUE_FOREVER = "LoopController.continue_forever";
+  private int loopCount = 0;
 
 
-	/****************************************
-	 * !ToDoo (Method description)
-	 *
-	 *@return   !ToDo (Return description)
-	 ***************************************/
-	public boolean getContinueForever()
-	{
-		return getPropertyAsBoolean(CONTINUE_FOREVER);
-	}
+  /****************************************
+   * !ToDo (Constructor description)
+   ***************************************/
+  public LoopController() {
+    setContinueForever(true);
+  }
 
-	public void initialize()
-	{
-		super.initialize();
-		resetLoopCount();
-	}
 
-	public void reInitialize()
-	{
-		super.reInitialize();
-		resetLoopCount();
-	}
+  /****************************************
+   * !ToDo (Method description)
+   *
+   *@param loops  !ToDo (Parameter description)
+   ***************************************/
+  public void setLoops(int loops) {
+    setProperty(LOOPS, new Integer(loops));
+  }
 
-	protected void incrementLoopCount()
-	{
-		loopCount++;
-	}
 
-	protected void resetLoopCount()
-	{
-		if(!getContinueForever() && getLoops() > -1)
-		{
-			this.setShortCircuit(true);
-		}
-		else
-		{
-			loopCount = 0;
-		}
-	}
+  /****************************************
+   * !ToDoo (Method description)
+   *
+   *@return   !ToDo (Return description)
+   ***************************************/
+  public int getLoops() {
+    return getPropertyAsInt(LOOPS);
+  }
 
-	protected boolean hasNextAtEnd()
-	{
-		resetCurrent();
-		incrementLoopCount();
-		if(endOfLoop())
-		{
-			return false;
-		}
-		else
-		{
-			return hasNext();
-		}
-	}
 
-	protected void nextAtEnd()
-	{
-		resetCurrent();
-		incrementLoopCount();
-	}
+  /****************************************
+   * !ToDo (Method description)
+   *
+   *@param forever  !ToDo (Parameter description)
+   ***************************************/
+  public void setContinueForever(boolean forever) {
+    setProperty(CONTINUE_FOREVER, new Boolean(forever));
+  }
 
-	private boolean endOfLoop()
-	{
-		return (!getContinueForever() || getLoops() > -1) && loopCount >= getLoops();
-	}
 
-	public static class Test extends junit.framework.TestCase
-	{
-		public Test(String name)
-		{
-			super(name);
-		}
+  /****************************************
+   * !ToDoo (Method description)
+   *
+   *@return   !ToDo (Return description)
+   ***************************************/
+  public boolean getContinueForever() {
+    return getPropertyAsBoolean(CONTINUE_FOREVER);
+  }
 
-		public void testProcessing() throws Exception
-		{
-			GenericController controller = new GenericController();
-			GenericController sub_1 = new GenericController();
-			sub_1.addTestElement(makeSampler("one"));
-			sub_1.addTestElement(makeSampler("two"));
-			controller.addTestElement(sub_1);
-			controller.addTestElement(makeSampler("three"));
-			LoopController sub_2 = new LoopController();
-			sub_2.setLoops(3);
-			GenericController sub_3 = new GenericController();
-			sub_2.addTestElement(makeSampler("four"));
-			sub_3.addTestElement(makeSampler("five"));
-			sub_3.addTestElement(makeSampler("six"));
-			sub_2.addTestElement(sub_3);
-			sub_2.addTestElement(makeSampler("seven"));
-			controller.addTestElement(sub_2);
-			String[] order = new String[]{"one","two","three","four","five","six","seven",
-						"four","five","six","seven","four","five","six","seven"};
-			int counter = 15;
-			for (int i = 0; i < 2; i++)
-			{
-				assertEquals(15,counter);
-				counter = 0;
-				while(controller.hasNext())
-				{
-					TestElement sampler = controller.next();
-					assertEquals(order[counter++],sampler.getProperty(TestElement.NAME));
-				}
-			}
-		}
 
-		private TestElement makeSampler(String name)
-		{
-		  	TestSampler s= new TestSampler();
-			s.setName(name);
-			return s;
-		}
-		class TestSampler extends AbstractSampler {
-		  public void addCustomTestElement(TestElement t) { }
-		  public org.apache.jmeter.samplers.SampleResult sample(org.apache.jmeter.samplers.Entry e) { return null; }
-		}
-	}
+  public void initialize() {
+    super.initialize();
+    resetLoopCount();
+  }
+
+
+  public void reInitialize() {
+    super.reInitialize();
+    resetLoopCount();
+  }
+
+
+  protected void incrementLoopCount() {
+    loopCount++;
+  }
+
+
+  protected void resetLoopCount() {
+    if (!getContinueForever() && getLoops() > -1) {
+      this.setShortCircuit(true);
+    } else {
+      loopCount = 0;
+    }
+  }
+
+
+  protected boolean hasNextAtEnd() {
+    resetCurrent();
+    incrementLoopCount();
+    if (endOfLoop()) {
+      return false;
+    } else {
+      return hasNext();
+    }
+  }
+
+
+  protected void nextAtEnd() {
+    resetCurrent();
+    incrementLoopCount();
+  }
+
+
+  private boolean endOfLoop() {
+    return (!getContinueForever() || getLoops() > -1) && loopCount >= getLoops();
+  }
+
+
+  public static class Test extends junit.framework.TestCase {
+
+    public Test(String name) {
+      super(name);
+    }
+
+
+    public void testProcessing() throws Exception {
+      GenericController controller = new GenericController();
+      GenericController sub_1 = new GenericController();
+      sub_1.addTestElement(makeSampler("one"));
+      sub_1.addTestElement(makeSampler("two"));
+      controller.addTestElement(sub_1);
+      controller.addTestElement(makeSampler("three"));
+      LoopController sub_2 = new LoopController();
+      sub_2.setLoops(3);
+      GenericController sub_3 = new GenericController();
+      sub_2.addTestElement(makeSampler("four"));
+      sub_3.addTestElement(makeSampler("five"));
+      sub_3.addTestElement(makeSampler("six"));
+      sub_2.addTestElement(sub_3);
+      sub_2.addTestElement(makeSampler("seven"));
+      controller.addTestElement(sub_2);
+      String[] order = new String[]{"one", "two", "three", "four", "five", "six", "seven",
+                                    "four", "five", "six", "seven", "four", "five", "six", "seven"};
+      int counter = 15;
+      for (int i = 0; i < 2; i++) {
+        assertEquals(15, counter);
+        counter = 0;
+        while (controller.hasNext()) {
+          TestElement sampler = controller.next();
+          assertEquals(order[counter++], sampler.getProperty(TestElement.NAME));
+        }
+      }
+    }
+
+
+    private TestElement makeSampler(String name) {
+      TestSampler s = new TestSampler();
+      s.setName(name);
+      return s;
+    }
+
+
+    class TestSampler extends AbstractSampler {
+
+      public void addCustomTestElement(TestElement t) {
+      }
+
+
+      public org.apache.jmeter.samplers.SampleResult sample(org.apache.jmeter.samplers.Entry e) {
+        return null;
+      }
+    }
+  }
 }
