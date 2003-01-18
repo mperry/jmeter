0f87b650d2bb9e14ce01c59d68794dcf148e6d66
Oliver Rossmueller <oros@apache.org>
2003-01-18 14:24:31 +0000

    reformatted + license
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jmeter/branches/refactorings_branch@323026 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/org/apache/jmeter/control/gui/LoopControlPanel.java
+++ src/core/org/apache/jmeter/control/gui/LoopControlPanel.java
@@ -1,4 +1,60 @@
+/*
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ * if any, must include the following acknowledgment:
+ * "This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/)."
+ * Alternately, this acknowledgment may appear in the software itself,
+ * if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ * "Apache JMeter" must not be used to endorse or promote products
+ * derived from this software without prior written permission. For
+ * written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ * "Apache JMeter", nor may "Apache" appear in their name, without
+ * prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
 package org.apache.jmeter.control.gui;
+
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,6 +75,7 @@ import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.gui.layout.VerticalLayout;
 
+
 /****************************************
  * Title: JMeter Description: Copyright: Copyright (c) 2000 Company: Apache
  *
@@ -27,228 +84,211 @@ import org.apache.jorphan.gui.layout.VerticalLayout;
  *@version   1.0
  ***************************************/
 
-public class LoopControlPanel extends AbstractControllerGui implements KeyListener, ActionListener
-{
+public class LoopControlPanel extends AbstractControllerGui implements KeyListener, ActionListener {
 
 
-	JCheckBox infinite;
-	JTextField loops;
+  JCheckBox infinite;
+  JTextField loops;
 
-	private boolean displayName = true;
-	private static String INFINITE = "Infinite Field";
-	private static String LOOPS = "Loops Field";
-
-	/****************************************
-	 * !ToDo (Constructor description)
-	 ***************************************/
-	public LoopControlPanel()
-	{
-		this(true);
-	}
-
-	/****************************************
-	 * !ToDo (Constructor description)
-	 *
-	 *@param displayName  !ToDo (Parameter description)
-	 ***************************************/
-	public LoopControlPanel(boolean displayName)
-	{
-		this.displayName = displayName;
-		init();
-		setState(1);
-	}
-
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@param element  !ToDo (Parameter description)
-	 ***************************************/
-	public void configure(TestElement element)
-	{
-		setName((String)element.getProperty(TestElement.NAME));
-		if(element instanceof LoopController)
-		{
-			setState(((LoopController)element).getLoops());
-		}
-		else
-		{
-			setState(1);
-		}
-	}
-
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@return   !ToDo (Return description)
-	 ***************************************/
-	public TestElement createTestElement()
-	{
-		LoopController lc = new LoopController();
-		configureTestElement(lc);
-		if(loops.getText().length() > 0)
-		{
-			lc.setLoops(Integer.parseInt(loops.getText()));
-		}
-		else
-		{
-			lc.setLoops(-1);
-		}
-		return lc;
-	}
-
-	/****************************************
-	 * !ToDo (Method description)
-	 *
-	 *@param event  !ToDo (Parameter description)
-	 ***************************************/
-	public void actionPerformed(ActionEvent event)
-	{
-		if(infinite.isSelected())
-		{
-			loops.setText("");
-			loops.setEnabled(false);
-		}
-		else
-		{
-			loops.setEnabled(true);
-			new FocusRequester(loops);
-		}
-	}
-
-	/****************************************
-	 * Description of the Method
-	 *
-	 *@param e  Description of Parameter
-	 ***************************************/
-	public void keyPressed(KeyEvent e) { }
-
-	/****************************************
-	 * Description of the Method
-	 *
-	 *@param e  Description of Parameter
-	 ***************************************/
-	public void keyTyped(KeyEvent e) { }
-
-	/****************************************
-	 * Description of the Method
-	 *
-	 *@param e  Description of Parameter
-	 ***************************************/
-	public void keyReleased(KeyEvent e)
-	{
-		String temp = e.getComponent().getName();
-		if(temp.equals(LOOPS))
-		{
-			try
-			{
-				Integer.parseInt(loops.getText());
-			}
-			catch(NumberFormatException ex)
-			{
-				if(loops.getText().length() > 0)
-				{
-					// We need a standard warning/error dialog. The problem with
-					// having it here is that the dialog is centered over this
-					// LoopControlPanel instead of begin centered in the entire
-					// JMeter GUI window.
-					JOptionPane.showMessageDialog(this, "You must enter a valid number",
-							"Invalid data", JOptionPane.WARNING_MESSAGE);
-					loops.setText("");
-				}
-			}
-		}
-	}
-
-	/****************************************
-	 * !ToDoo (Method description)
-	 *
-	 *@return   !ToDo (Return description)
-	 ***************************************/
-	public String getStaticLabel()
-	{
-		return JMeterUtils.getResString("loop_controller_title");
-	}
-
-	private void init()
-	{
-		// The Loop Controller panel can be displayed standalone or inside another panel.
-		// For standalone, we want to display the TITLE, NAME, etc. (everything). However,
-		// if we want to display it within another panel, we just display the Loop Count
-		// fields (not the TITLE and NAME).
-
-		// Standalone
-		if(displayName)
-		{
-			this.setLayout(new VerticalLayout(5, VerticalLayout.LEFT, VerticalLayout.TOP));
-
-			// MAIN PANEL
-			JPanel mainPanel = new JPanel();
-			Border margin = new EmptyBorder(10, 10, 5, 10);
-			mainPanel.setBorder(margin);
-			mainPanel.setLayout(new VerticalLayout(5, VerticalLayout.LEFT));
-
-			// TITLE
-			JLabel panelTitleLabel = new JLabel(JMeterUtils.getResString("loop_controller_title"));
-			Font curFont = panelTitleLabel.getFont();
-			int curFontSize = curFont.getSize();
-			curFontSize += 4;
-			panelTitleLabel.setFont(new Font(curFont.getFontName(), curFont.getStyle(), curFontSize));
-			mainPanel.add(panelTitleLabel);
-
-			// NAME
-			mainPanel.add(getNamePanel());
-
-			// LOOP
-			mainPanel.add(createLoopCountPanel());
-
-			this.add(mainPanel);
-		}
-
-		// Embedded
-		else
-		{
-			this.add(createLoopCountPanel());
-		}
-	}
+  private boolean displayName = true;
+  private static String INFINITE = "Infinite Field";
+  private static String LOOPS = "Loops Field";
 
 
-	private JPanel createLoopCountPanel()
-	{
-		JPanel loopPanel = new JPanel();
+  /****************************************
+   * !ToDo (Constructor description)
+   ***************************************/
+  public LoopControlPanel() {
+    this(true);
+  }
 
-		// LOOP LABEL
-		JLabel loopsLabel = new JLabel(JMeterUtils.getResString("iterator_num"));
-		loopPanel.add(loopsLabel);
 
-		// TEXT FIELD
-		loops = new JTextField(5);
-		loopPanel.add(loops);
-		loops.setName(LOOPS);
-		loops.addKeyListener(this);
-		loops.setText("1");
+  /****************************************
+   * !ToDo (Constructor description)
+   *
+   *@param displayName  !ToDo (Parameter description)
+   ***************************************/
+  public LoopControlPanel(boolean displayName) {
+    this.displayName = displayName;
+    init();
+    setState(1);
+  }
 
-		// FOREVER CHECKBOX
-		infinite = new JCheckBox(JMeterUtils.getResString("infinite"));
-		infinite.setActionCommand(INFINITE);
-		infinite.addActionListener(this);
-		loopPanel.add(infinite);
 
-		return loopPanel;
-	}
+  /****************************************
+   * !ToDo (Method description)
+   *
+   *@param element  !ToDo (Parameter description)
+   ***************************************/
+  public void configure(TestElement element) {
+    setName((String)element.getProperty(TestElement.NAME));
+    if (element instanceof LoopController) {
+      setState(((LoopController)element).getLoops());
+    } else {
+      setState(1);
+    }
+  }
 
-	private void setState(int loopCount)
-	{
-		if(loopCount <= -1)
-		{
-			infinite.setSelected(true);
-			loops.setEnabled(false);
-			loops.setText("");
-		}
-		else
-		{
-			infinite.setSelected(false);
-			loops.setEnabled(true);
-			loops.setText("" + loopCount);
-		}
-	}
+
+  /****************************************
+   * !ToDo (Method description)
+   *
+   *@return   !ToDo (Return description)
+   ***************************************/
+  public TestElement createTestElement() {
+    LoopController lc = new LoopController();
+    configureTestElement(lc);
+    if (loops.getText().length() > 0) {
+      lc.setLoops(Integer.parseInt(loops.getText()));
+    } else {
+      lc.setLoops(-1);
+    }
+    return lc;
+  }
+
+
+  /****************************************
+   * !ToDo (Method description)
+   *
+   *@param event  !ToDo (Parameter description)
+   ***************************************/
+  public void actionPerformed(ActionEvent event) {
+    if (infinite.isSelected()) {
+      loops.setText("");
+      loops.setEnabled(false);
+    } else {
+      loops.setEnabled(true);
+      new FocusRequester(loops);
+    }
+  }
+
+
+  /****************************************
+   * Description of the Method
+   *
+   *@param e  Description of Parameter
+   ***************************************/
+  public void keyPressed(KeyEvent e) {
+  }
+
+
+  /****************************************
+   * Description of the Method
+   *
+   *@param e  Description of Parameter
+   ***************************************/
+  public void keyTyped(KeyEvent e) {
+  }
+
+
+  /****************************************
+   * Description of the Method
+   *
+   *@param e  Description of Parameter
+   ***************************************/
+  public void keyReleased(KeyEvent e) {
+    String temp = e.getComponent().getName();
+    if (temp.equals(LOOPS)) {
+      try {
+        Integer.parseInt(loops.getText());
+      } catch (NumberFormatException ex) {
+        if (loops.getText().length() > 0) {
+          // We need a standard warning/error dialog. The problem with
+          // having it here is that the dialog is centered over this
+          // LoopControlPanel instead of begin centered in the entire
+          // JMeter GUI window.
+          JOptionPane.showMessageDialog(this, "You must enter a valid number",
+                                        "Invalid data", JOptionPane.WARNING_MESSAGE);
+          loops.setText("");
+        }
+      }
+    }
+  }
+
+
+  /****************************************
+   * !ToDoo (Method description)
+   *
+   *@return   !ToDo (Return description)
+   ***************************************/
+  public String getStaticLabel() {
+    return JMeterUtils.getResString("loop_controller_title");
+  }
+
+
+  private void init() {
+    // The Loop Controller panel can be displayed standalone or inside another panel.
+    // For standalone, we want to display the TITLE, NAME, etc. (everything). However,
+    // if we want to display it within another panel, we just display the Loop Count
+    // fields (not the TITLE and NAME).
+
+    // Standalone
+    if (displayName) {
+      this.setLayout(new VerticalLayout(5, VerticalLayout.LEFT, VerticalLayout.TOP));
+
+      // MAIN PANEL
+      JPanel mainPanel = new JPanel();
+      Border margin = new EmptyBorder(10, 10, 5, 10);
+      mainPanel.setBorder(margin);
+      mainPanel.setLayout(new VerticalLayout(5, VerticalLayout.LEFT));
+
+      // TITLE
+      JLabel panelTitleLabel = new JLabel(JMeterUtils.getResString("loop_controller_title"));
+      Font curFont = panelTitleLabel.getFont();
+      int curFontSize = curFont.getSize();
+      curFontSize += 4;
+      panelTitleLabel.setFont(new Font(curFont.getFontName(), curFont.getStyle(), curFontSize));
+      mainPanel.add(panelTitleLabel);
+
+      // NAME
+      mainPanel.add(getNamePanel());
+
+      // LOOP
+      mainPanel.add(createLoopCountPanel());
+
+      this.add(mainPanel);
+    }
+
+    // Embedded
+    else {
+      this.add(createLoopCountPanel());
+    }
+  }
+
+
+  private JPanel createLoopCountPanel() {
+    JPanel loopPanel = new JPanel();
+
+    // LOOP LABEL
+    JLabel loopsLabel = new JLabel(JMeterUtils.getResString("iterator_num"));
+    loopPanel.add(loopsLabel);
+
+    // TEXT FIELD
+    loops = new JTextField(5);
+    loopPanel.add(loops);
+    loops.setName(LOOPS);
+    loops.addKeyListener(this);
+    loops.setText("1");
+
+    // FOREVER CHECKBOX
+    infinite = new JCheckBox(JMeterUtils.getResString("infinite"));
+    infinite.setActionCommand(INFINITE);
+    infinite.addActionListener(this);
+    loopPanel.add(infinite);
+
+    return loopPanel;
+  }
+
+
+  private void setState(int loopCount) {
+    if (loopCount <= -1) {
+      infinite.setSelected(true);
+      loops.setEnabled(false);
+      loops.setText("");
+    } else {
+      infinite.setSelected(false);
+      loops.setEnabled(true);
+      loops.setText("" + loopCount);
+    }
+  }
 }
